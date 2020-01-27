d10353d64a3298210b2672e9fd7521e9a8ed5ae0
Spühler Stefan <git@tuxli.ch>
2020-01-27 19:49:53 +0100

    Add tmp_path to save MRT dumps in a different directory

--- import_bgpdump.py
+++ import_bgpdump.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 import re
 import psycopg2
 from psycopg2 import extras
@@ -8,16 +9,17 @@ from bgpdumpy import BGPDump, TableDumpV2
 import requests
 from asn_app.utils import load_config
 
-def download_file(url):
+def download_file(url, tmp_path):
     """ https://stackoverflow.com/a/16696317 """
     local_filename = url.split('/')[-1]
+    full_path = os.path.join(tmp_path, local_filename)
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
-        with open(local_filename, 'wb') as f:
+        with open(full_path, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
                 if chunk: # filter out keep-alive new chunks
                     f.write(chunk)
-    return local_filename
+    return full_path
 
 def insert_into(db_conn, cursor, prefix_list):
     """ Insert prefixlist into database """
@@ -75,7 +77,7 @@ def main():
     insert_cur = db_conn.cursor()
     i = 0
     prefix_list = set()
-    file_name = download_file(cfg['bgp']['url'])
+    file_name = download_file(cfg['bgp']['url'], cfg['bgp']['tmp_path'])
     with BGPDump(file_name) as bgp:
         for entry in bgp:
 
