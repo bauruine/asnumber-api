a12af0d1231c1fdc5554af51291c310150d08e73
Spühler Stefan <git@tuxli.ch>
2020-06-22 20:16:44 +0200

    update db schema

--- db/postgresql_db.sql
+++ db/postgresql_db.sql
@@ -3,27 +3,28 @@ CREATE TABLE asnumbers (
     asname character varying(255),
     asdescription character varying(255),
     country character varying(255),
-    rir character varying(255)
+    rir character varying(255),
+    last_updated timestamp without time zone
 );
 
 
 CREATE TABLE prefixes (
-	prefix_id serial PRIMARY KEY,
-    prefix cidr,
+    prefix cidr PRIMARY KEY,
     added_timestamp timestamp without time zone
 );
 
 
 CREATE TABLE asnumbers_prefixes (
 	asnumber bigint REFERENCES asnumbers(asnumber) ON UPDATE CASCADE,
-	prefix_id bigint REFERENCES prefixes(prefix_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	prefix cidr REFERENCES prefixes(prefix) ON UPDATE CASCADE ON DELETE CASCADE,
 	first_seen timestamp without time zone,
 	last_seen timestamp without time zone, 
-	CONSTRAINT asnumber_prefix_primary_key PRIMARY KEY (asnumber, prefix_id)
+	CONSTRAINT asnumber_prefix_primary_key PRIMARY KEY (asnumber, prefix)
 
 );
 
 CREATE UNIQUE INDEX asnumber_idx ON asnumbers USING btree (asnumber);
+CREATE UNIQUE INDEX prefixes_idx ON prefixes USING btree (prefix);
 
 
 CREATE INDEX prefix_idx ON prefixes USING gist (prefix inet_ops);
@@ -34,4 +35,3 @@ ALTER TABLE public.asnumbers OWNER TO asnumber;
 ALTER TABLE public.asnumbers_prefixes OWNER TO asnumber;
 
 ALTER TABLE public.prefixes OWNER TO asnumber;
-
