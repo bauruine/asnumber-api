a88dfbe90fa20fe8366e36475c0109476b2e5caf
Spühler Stefan <git@tuxli.ch>
2020-06-23 13:03:31 +0200

    fix config file load

--- asn_app/utils.py
+++ asn_app/utils.py
@@ -3,7 +3,7 @@ import yaml
 
 def load_config(config_file):
     # load config file
-    config_file = os.path.join(os.path.dirname(__file__), config_file)
+    config_file = os.path.join(os.path.dirname(__file__), '../', config_file)
     with open(config_file, 'r') as ymlfile:
         cfg = yaml.safe_load(ymlfile)
         return cfg
