ab3b9fa11a1b2735882d0010417c4cf1f5fa9747
Spühler Stefan <git@tuxli.ch>
2020-07-09 11:25:35 +0200

    Search ASN description with whois

--- asn_app/asn.py
+++ asn_app/asn.py
@@ -1,5 +1,6 @@
 """ Script to download asnumber information """
 import re
+import subprocess
 import requests
 import dns.resolver
 
@@ -26,6 +27,16 @@ def get_asdesc_peeringdb(asn, req_session):
             return description
     return ''
 
+def get_asdesc_whois(asn):
+    """ return the ASN description from peeringdb """
+    result = subprocess.run(['/usr/bin/whois', f'AS{asn}'], capture_output=True)
+    try:
+        parsed = re.search(r'(OrgName|owner|org-name):\W+(.+?)\n', result.stdout.decode())
+    except UnicodeDecodeError as e:
+        return ''
+    if parsed:
+        return parsed.group(2)
+    return ""
 
 def add_asn(db_conn, resolver, req_session, asn):
 
@@ -45,11 +56,15 @@ def add_asn(db_conn, resolver, req_session, asn):
                     description = as_desc
                     print(f'{asn} peeringdb with desctiption: {description}')
                 else:
-                    description = get_asdesc_ripe(asn, req_session, regex)
+                    description = get_asdesc_whois(asn)
                     if description:
-                        print(f'{asn} ripencc with description: {description}')
+                        print(f'{asn} whois with description: {description}')
                     else:
-                        print(f'{asn} could not find as description')
+                        description = get_asdesc_ripe(asn, req_session, regex)
+                        if description:
+                            print(f'{asn} ripencc with description: {description}')
+                        else:
+                            print(f'{asn} could not find as description')
                 insert_cur.execute("UPDATE asnumbers set asnumber = %s, asname = %s, asdescription = %s, country = %s, rir = %s WHERE asnumber = %s", (asn, parsed.group(4), description, parsed.group(1), parsed.group(2), asn))
 
                 db_conn.commit()
